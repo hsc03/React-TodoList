373213d2441c7cf648d9d32d0aed4536506af1c6
Seongcheol Hwang <hsc0710@naver.com>
2024-01-22 16:00:42 +0900

    Complete TodoItem Configuration

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Form, Button } from 'react-bootstrap';
 
 const Header = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const inputChange = (e) => {
+        setInputValue(e.target.value)
+    };
+
     return (
         <div className="headerContainer">
             <div className="headerTitle">
                 <h2>Todo-List</h2>
             </div>
             <div className="headerInput">
-                <Form.Control type="text" placeholder="할 일을 입력해주세요." />
+                <Form.Control
+                    type="text"
+                    value={inputValue}
+                    onChange={inputChange}
+                    placeholder="할 일을 입력해주세요."
+                />
                 <Button type="submit" size="sm">추가</Button>
             </div>
         </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import '../styles/TodoItem.css';
+import { Button } from 'react-bootstrap';
 
 const TodoItem = () => {
     return (
         <div className="itemContainer">
-            <div className="content">
-                <span>testext</span>
+            <div className="text">
+                <span>테스트 중인 그저 그런 텍스트</span>
             </div>
-            
+            <div className="buttons">
+                <Button variant="outline-danger">삭제</Button>
+                <Button variant="outline-warning">수정</Button>
+                <Button variant="outline-success">완료</Button>
+            </div>
         </div>
     );
 };
