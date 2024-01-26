2d54c2dab4e067679bada3e331917b8527743bf9
hsc03 <hsc0710@naver.com>
2024-01-26 13:54:51 +0900

    Changed TodoItem structure & Added completion button feature

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/TodoItem.css';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 const TodoItem = ({ inputValue }) => {
+    const [isCompleted, setIsCompleted] = useState(false);
+
+    const handleCompleteChange = (e) => {
+        setIsCompleted(e.currentTarget.checked);
+    };
+
+    const handleDelete = () => {
+        alert("삭제됩니다.(구현x)");
+    }
+
+    const handleEdit = () => {
+        alert("수정됩니다.(구현 x)");
+    }
+
     return (
-        <div className="itemContainer">
-            <div className="text">
-                <span>{inputValue}</span>
+        <div className="itemContainer"style={{ backgroundColor : isCompleted ? 'grey' : 'white'}}>
+            <div className="itemTop">
+                <div className="text">
+                    <span>{inputValue}</span>
+                </div>
+                <Form.Check 
+                    type="switch"
+                    id={`complete-switch-${inputValue}`}
+                    label=""
+                    checked={isCompleted}
+                    onChange={handleCompleteChange}
+                />
             </div>
-            <div className="buttons">
-                <Button variant="outline-danger" size="sm">삭제</Button>
-                <Button variant="outline-warning" size="sm">수정</Button>
-                <Button variant="outline-success" size="sm">완료</Button>
+            <div className="itemBottom">
+                <Button variant="outline-danger" onClick={handleDelete}>삭제</Button>
+                <Button variant="outline-warning" disabled={isCompleted} onClick={handleEdit}>수정</Button>
             </div>
         </div>
     );
